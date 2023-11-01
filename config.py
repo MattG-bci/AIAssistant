ce7ef0c4eac373abe631a6e08b597990cf8335e4
MattG-bci <mateusz.f.grzybowski@gmail.com>
2023-11-01 22:38:36 +0000

    Initialising LLM project

---
+++ config.py
@@ -0,0 +1,7 @@
+LLM_CONFIGURATION = dict(
+    model=dict(
+        model_name="gpt-3.5-turbo",
+        system_role=dict(role="system",
+                        content="You are a machine learning expert..")
+    )
+)
---
+++ generate_answer.py
@@ -0,0 +1,21 @@
+import openai
+
+from config import LLM_CONFIGURATION as config
+
+def parse_query(query):
+    completions = openai.ChatCompletion.create(
+                model=config["model"]["model_name"],
+                messages=[
+                        config["model"]["system_role"],
+                        {"role": "user", "content": f"{query}"},
+                    ]
+            )
+    response = completions['choices'][0]['message']['content']
+    return response
+
+
+if __name__ == "__main__":
+    query = "What is the most important concept in deep learning?"
+    response = parse_query(query)
+    print(response)
+
